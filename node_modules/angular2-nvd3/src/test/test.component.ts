613891ec7d0f903981b0d0bbfbb02593546b13a0
Brianna Dickman <brianna.dickman@gmail.com>
2018-01-06 13:11:55 -0600

    merge pull from master

--- node_modules/angular2-nvd3/src/test/test.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, DebugElement } from '@angular/core';
-
-@Component({ template: `<div appNvD3 [options]="options" [data]="data"></div>` })
-/** experimanetalDecorator */
-export class TestComponent {
-  public options: any;
-  public data: any;
-}
